eb73f1f44417ccd583958c5e1628584bfd213f9c
otgonbileg11 <otgonbileg1030@gmail.com>
2023-05-09 20:40:51 +0800

    wtf 2

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -30,7 +30,7 @@
           <li>
             <NuxtLink to="/events/create">
               <svg class="md:hidden w-5 fill-accent dark:fill-slate-200 hover:fill-primary" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M256 80c0-17.7-14.3-32-32-32s-32 14.3-32 32V224H48c-17.7 0-32 14.3-32 32s14.3 32 32 32H192V432c0 17.7 14.3 32 32 32s32-14.3 32-32V288H400c17.7 0 32-14.3 32-32s-14.3-32-32-32H256V80z"/></svg>
-              <span class="hidden md:block font-semibold text-accent dark:text-slate-200 hover:text-primary dark:hover:text-alternate">Create event</span>
+              <span class="hidden md:block font-semibold text-accent dark:text-slate-200 hover:text-primary dark:hover:text-alternate">Create</span>
             </NuxtLink>
           </li>
           <li v-show="!isLoggedIn">
